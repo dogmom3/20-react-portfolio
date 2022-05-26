aeea6e1d22cbfc2bf4e86b8ab742d3366dc14179
dogmom3 <blair.seivers@yahoo.com>
2022-05-26 12:03:38 -0400

    fixed error with portfolio page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,39 +1,64 @@
 import React from "react";
 
 export default function Blog() {
-//   const projects = [
-//     {
-//       name: "Weather App",
-//       description:
-//         "An app that allows you to search for weather forecasts by city and maintains your search history on the page.",
-//     },
-//     {
-//       name: "Password Generator",
-//       description:
-//         "Generates a random password based on criteria selected by the user.",
-//     },
-//     {
-//       name: "Quiz",
-//       description:
-//         "An app that asks a series of questions that must be answered within a given timeframe and then keeps track of your score based on correctly answered questions.",
-//     },
-//     {
-//       name: "Employee Team Profile Builder",
-//       description:
-//         "A way to input data on employee or team members and then collectively view all of those profiles on one page.",
-//     },
-//     {
-//       name: "Happy Trees",
-//       description:
-//         "This app is useful for mitigation companies to input and track data on trees planted."
-//     }
-//   ];
-//   function projectSelected(name) {
-//     console.log(`${name} clicked`)
-//   }
+  const projects = [
+    {
+      name: "Weather App",
+      description:
+        "An app that allows you to search for weather forecasts by city and maintains your search history on the page.",
+    },
+    {
+      name: "Password Generator",
+      description:
+        "Generates a random password based on criteria selected by the user.",
+    },
+    {
+      name: "Quiz",
+      description:
+        "An app that asks a series of questions that must be answered within a given timeframe and then keeps track of your score based on correctly answered questions.",
+    },
+    {
+      name: "Employee Team Profile Builder",
+      description:
+        "A way to input data on employee or team members and then collectively view all of those profiles on one page.",
+    },
+    {
+      name: "Happy Trees",
+      description:
+        "This app is useful for mitigation companies to input and track data on trees planted."
+    }
+  ];
+  function projectSelected(name) {
+    console.log(`${name} clicked`)
+  }
   return (
     <div>
+<h1>Portfolio:</h1>
 
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="projects">
+            {" "}
+           📸
+         </span>{" "}
+          Oh Snap!
+         </a>
+       </h2>
+
+       <div>
+         <ul className="flex-row">
+         {projects.map((project) => (
+  <li
+    className="mx-1"
+    key={project.name}
+  >
+    <span onClick={() => projectSelected(project.name)} >
+      {project.name}
+    </span>
+  </li>
+))}
+        </ul>
+      </div>
     </div>
   );
 }
