6ff1b3f94f1f422afc1ae046649b1454de8a93c8
dogmom3 <blair.seivers@yahoo.com>
2022-06-05 23:21:57 -0400

    uupdate about page styles

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import Footer from './Footer';
+// import Header from './Header';
 
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
@@ -32,6 +29,9 @@ export default function PortfolioContainer() {
 
   return (
     <div>
+      {/* <header>
+        <Header />
+      </header> */}
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
